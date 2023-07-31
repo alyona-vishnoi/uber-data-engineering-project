3a6dde3c493426b88e6945ec9e7509a654bfc129
Alyona Vishnoi <88257366+alyona-vishnoi@users.noreply.github.com>
2023-07-30 22:23:18 -0400

    Update analytics_query.sql

--- analytics_query.sql
+++ analytics_query.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `data-with-darshil.uber_data_engineering_yt.tbl_analytics` AS (
+CREATE OR REPLACE TABLE `datawithalyona.uber_de.tbl_analytics` AS (
 SELECT 
 f.trip_id
 f.VendorID,
@@ -21,12 +21,12 @@ f.improvement_surcharge,
 f.total_amount
 FROM 
 
-`data-with-darshil.uber_data_engineering_yt.fact_table` f
-JOIN `data-with-darshil.uber_data_engineering_yt.datetime_dim` d  ON f.datetime_id=d.datetime_id
-JOIN `data-with-darshil.uber_data_engineering_yt.passenger_count_dim` p  ON p.passenger_count_id=f.passenger_count_id  
-JOIN `data-with-darshil.uber_data_engineering_yt.trip_distance_dim` t  ON t.trip_distance_id=f.trip_distance_id  
-JOIN `data-with-darshil.uber_data_engineering_yt.rate_code_dim` r ON r.rate_code_id=f.rate_code_id  
-JOIN `data-with-darshil.uber_data_engineering_yt.pickup_location_dim` pick ON pick.pickup_location_id=f.pickup_location_id
-JOIN `data-with-darshil.uber_data_engineering_yt.dropoff_location_dim` drop ON drop.dropoff_location_id=f.dropoff_location_id
-JOIN `data-with-darshil.uber_data_engineering_yt.payment_type_dim` pay ON pay.payment_type_id=f.payment_type_id)
+`data-with-datawithalyona.uber_de.fact_table` f
+JOIN `datawithalyona.uber_de.uber_de.datetime_dim` d  ON f.datetime_id=d.datetime_id
+JOIN `datawithalyona.uber_de.passenger_count_dim` p  ON p.passenger_count_id=f.passenger_count_id  
+JOIN `datawithalyona.uber_de.trip_distance_dim` t  ON t.trip_distance_id=f.trip_distance_id  
+JOIN `datawithalyona.uber_de.rate_code_dim` r ON r.rate_code_id=f.rate_code_id  
+JOIN `datawithalyona.uber_de.pickup_location_dim` pick ON pick.pickup_location_id=f.pickup_location_id
+JOIN `datawithalyona.uber_de.dropoff_location_dim` drop ON drop.dropoff_location_id=f.dropoff_location_id
+JOIN `datawithalyona.uber_de.payment_type_dim` pay ON pay.payment_type_id=f.payment_type_id)
 ;
